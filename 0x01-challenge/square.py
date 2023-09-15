f458447d87cf994093053405a8990f12fd3264db
jeshi2 <jamesvean19@gmail.com>
2023-09-15 13:51:42 +0300

    fixing

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,5 +25,5 @@ if __name__ == "__main__":
 
     s = Square(width=12, height=9)
     print(s)
-    print("Area: ",s.area_of_my_square())
-    print("Perimeter: ",s.PermiterOfMySquare())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
